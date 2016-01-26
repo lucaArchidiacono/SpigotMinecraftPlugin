756472d7c551c2fb952f45fe89c961dc2ce0eeef
Luca Archidiacono <unconfigured@null.spigotmc.org>
2016-01-26 11:15:06 +0100

    Initial commit

---
+++ src/org/spigotmc/tutorial/Main.java
@@ -0,0 +1,20 @@
+package org.spigotmc.tutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+
+
+
+}
